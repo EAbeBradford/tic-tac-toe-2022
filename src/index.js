06eec734c8929a3494f81fbdb4c07d9649bd98e6
Mx. Bradford <emma_bradford@milton.com>
2022-11-01 15:11:46 -0400

    added basic tic tac toe game

--- src/index.js
+++ src/index.js
@@ -3,6 +3,61 @@ import ReactDOM  from "react-dom/client";
 import './index.css';
 import ShoppingList from "./ShoppingList";
 
+class Game extends React.Component{
+    render() {
+        return(
+            <div className = "game">
+                <div>
+                {/* <div>hellooooooooooooooooooooo</div> */}
+
+                    <Board />
+                </div>
+            </div>
+        );
+    }
+}
+class Board extends React.Component {
+    
+    renderSquare(i)
+    {
+        return <Square />
+    }
+
+    render(){
+        const status = 'X'
+        return(
+            <div className="board">
+                <div>Next Player: {status}</div>
+                <div className="first-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="second-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="last-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Square extends React.Component
+{
+    render(){
+        return (
+        <button className="square">
+            {/*todo*/}
+        </button>
+        );
+    }
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<ShoppingList name="Mx. Bradford" item="the next MXTX book"/>);
+root.render(<Game />);
